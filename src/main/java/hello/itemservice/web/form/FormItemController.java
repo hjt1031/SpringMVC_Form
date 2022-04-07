88fdac670719fba8883792a4926ed974fad6447a
hjt1031 <hjt1031@naver.com>
2022-04-07 09:31:34 +0900

    체크박스 멀티

--- src/main/java/hello/itemservice/web/form/FormItemController.java
+++ src/main/java/hello/itemservice/web/form/FormItemController.java
@@ -9,7 +9,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 @Slf4j
 @Controller
@@ -18,6 +20,17 @@ import java.util.List;
 public class FormItemController {
 
     private final ItemRepository itemRepository;
+
+    //컨트롤러 호출 시 어떤 맵핑을 호출하던 간에 모델에 담긴다.
+    @ModelAttribute("regions")
+    public Map<String, String> regions() {
+        // 해쉬맵은 순서보장이 안되서 LinkedHashMap 을 사용
+        Map<String, String> regions = new LinkedHashMap<>();
+        regions.put("SEOUL","서울");
+        regions.put("BUSAN","부산");
+        regions.put("JEJU","제주");
+        return regions;
+    }
 
     @GetMapping
     public String items(Model model) {
@@ -30,6 +43,7 @@ public class FormItemController {
     public String item(@PathVariable long itemId, Model model) {
         Item item = itemRepository.findById(itemId);
         model.addAttribute("item", item);
+
         return "form/item";
     }
 
@@ -44,6 +58,8 @@ public class FormItemController {
     public String addItem(@ModelAttribute Item item, RedirectAttributes redirectAttributes) {
         // 체크박스가 체크가 안되어서 넘어 오면 null 로 주입됨. (= open 이라는 필드자체가 서버로 전송되지 않음.)
         log.info("item.open={}", item.getOpen());
+        // 서울 부산 제주 체크박스 보기
+        log.info("item.regions={}", item.getRegions());
         Item savedItem = itemRepository.save(item);
         redirectAttributes.addAttribute("itemId", savedItem.getId());
         redirectAttributes.addAttribute("status", true);
